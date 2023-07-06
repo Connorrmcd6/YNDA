235b95d069cf95f1b301bdc097704cd1607c705d
Connor McDonald <connormcd98@gmail.com>
2023-07-06 09:06:22 +0200

    adding kpis and charts

--- app.py
+++ app.py
@@ -5,13 +5,14 @@ st.set_page_config(page_title='YNDA', layout='wide',
                    initial_sidebar_state='collapsed')
 
 with st.sidebar:
-    names = get_names()
-    nominee = st.selectbox(
-        'Pick a person to drink:',
-        names)
-    if st.button("Update Dashboard"):
-        # displays the nominee
-        st.text(nominee)
+    x = 1
+    # names = get_names()
+    # nominee = st.selectbox(
+    #     'Pick a person to drink:',
+    #     names)
+    # if st.button("Update Dashboard"):
+    #     # displays the nominee
+    #     st.text(nominee)
 
 
 regular_league, spacer1, upcoming_fixtures, spacer2, previous_results, spacer3, current_standings = st.columns([
@@ -21,13 +22,33 @@ with regular_league:
     st.markdown("Regular League")
     st.divider()
 
-    with st.container():
-        st.write("This is inside the container")
+    metric_container = st.container()
+    chart_1_container = st.container()
+    chart_2_container = st.container()
 
-        # You can call any Streamlit command, including custom components:
+    with metric_container:
+        col1, col2, col3 = st.columns(3)
+
+        # row 1
+        col1.metric("Temperature", "70 °F", "1.2 °F")
+        col2.metric("Wind", "9 mph", "-8%")
+        col3.metric("Humidity", "86%", "4%")
+
+        # row 2
+        col1.metric("Temperature", "23 °C", "-4.8 °F")
+        col2.metric("Wind", "16 kph", "+4%")
+        col3.metric("Humidity", "65%", "-12%")
+        st.divider()
+
+    with chart_1_container:
+        st.write("Total Drinks")
         st.bar_chart(np.random.randn(50, 3))
+        st.divider()
 
-        st.write("This is outside the container")
+    with chart_2_container:
+        st.write("Rank Chart Last 10 Game Weeks")
+        data = np.random.randn(10, 1)
+        st.line_chart(data)
 
 with upcoming_fixtures:
     st.markdown("Upcoming Fixtures")
--- functions.py
+++ functions.py
@@ -7,7 +7,6 @@ from config import *
 def get_names():
     with requests.Session() as session:
         league_response = session.get(league_endpoint).json()
-        manager_names = pd.DataFrame.from_records(league_response['standings']['results'], columns=[
-                                                  'id', 'player_name']).set_index('id'). sort_values("player_name")
+        manager_names = pd.DataFrame.from_records(league_response['standings']['results'], columns=['id', 'player_name']).set_index('id'). sort_values("player_name")
     return manager_names
 
