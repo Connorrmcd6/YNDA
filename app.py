dbe3274f06e0ac58a940f39e54456de093b5eded
Connorrmcd6 <connormcd98@gmail.com>
2024-08-20 07:07:21 +0200

    updated cache function

--- app.py
+++ app.py
@@ -443,7 +443,7 @@ with rules_tab.expander("⏱️ Next Refresh", expanded= True):
         most_litres.clear()
         build_laps.clear()
         time_since_last_update.clear()
-        st.experimental_rerun()
+        st.rerun()
 
     st.code(f'status:\ngame week finished: {finished}\ngame week data checked: {checked}')
 
